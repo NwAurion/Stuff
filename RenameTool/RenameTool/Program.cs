12fb83623f8c3642b791a7b936529e9ac54870c1
NwAurion <aurion@gmail.com>
2014-03-22 04:33:32 +0100

    Full commit of local projects

---
+++ RenameTool/RenameTool/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace RenameTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter directory of the files to rename");
+            string dir = Console.ReadLine();
+            DirectoryInfo di = new DirectoryInfo(dir);
+            foreach (System.IO.FileInfo file in di.GetFiles("*.*"))
+            {
+                if (file.Extension.Equals(".part"))
+                {
+                    string oldFileName = file.Name.Substring(0, file.Name.IndexOf("."));
+                    int number = Convert.ToInt16(file.Name.Substring(oldFileName.Length + 1, file.Name.IndexOf(".zip") - oldFileName.Length - 1));
+                    number++;
+                    string numberAsString = number.ToString("000");
+                    string newFileName = oldFileName + "." + numberAsString;
+                    file.MoveTo(di.FullName + "/" + newFileName);
+                }
+            }
+        }
+    }
+}
